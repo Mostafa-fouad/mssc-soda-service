b3b329e6d436d4a8f118b7ae241ffe1efacb39e1
Mostafa Fouad <m.fouad_95@hotmail.com>
2021-03-31 00:26:33 +0200

    add soda controller closes #2

---
+++ src/main/java/com/microservices/msscsodaservice/web/controller/SodaController.java
@@ -0,0 +1,32 @@
+package com.microservices.msscsodaservice.web.controller;
+
+import com.microservices.msscsodaservice.web.model.SodaDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/soda")
+@RestController
+public class SodaController {
+    @GetMapping("/{sodaId}")
+    public ResponseEntity getSodaById(@PathVariable UUID sodaId){
+        //Todo impl
+        return new ResponseEntity(SodaDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewSoda(@RequestBody SodaDto sodaDto){
+
+        //Todo Impl
+        return new ResponseEntity(SodaDto.builder().build(),HttpStatus.CREATED);
+    }
+
+    @PutMapping("{/sodaId}")
+    public ResponseEntity updateSodaById (@PathVariable UUID sodaId, @RequestBody SodaDto sodaDto){
+
+        //Todo Impl
+        return new ResponseEntity(SodaDto.builder().build(),HttpStatus.NO_CONTENT);
+    }
+}
