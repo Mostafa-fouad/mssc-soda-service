e75c12c4e4e971b45075ec50753b747c69225009
Mostafa Fouad <m.fouad_95@hotmail.com>
2021-06-18 18:05:39 +0200

    add the new dummy change

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Spring Boot microservice example
 
 ## Merge and Diff Tool
+ add some sentences to test diff tool
+ add more sentences
+ changes for the new branch
--- src/test/java/com/microservices/msscsodaservice/web/controller/SodaControllerTest.java
+++ src/test/java/com/microservices/msscsodaservice/web/controller/SodaControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,12 +18,18 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(RestDocumentationExtension.class)
 @AutoConfigureRestDocs
 @WebMvcTest(SodaController.class)
+@ComponentScan(basePackages = "com.microservices.msscsodaservice.web.mapper")
 class SodaControllerTest {
 
     @Autowired
@@ -33,9 +40,29 @@ class SodaControllerTest {
 
     @Test
     void getSodaById() throws Exception {
-        mockMvc.perform(get("/api/v1/soda/"+ UUID.randomUUID().toString())
+        mockMvc.perform(get("/api/v1/soda/{sodaId}", UUID.randomUUID().toString())
+                .param("iscold","yes") // just added to see how we can document query parameters but actually we don't have query params in our controller
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(document("v1/soda",
+                            pathParameters(
+                             parameterWithName("sodaId").description("UUID of the desired soda to get.") //to document path parameters
+                            ),
+                            requestParameters(
+                                    parameterWithName("iscold").description("the temperature of the soda") //to document Query parameters
+                            ),
+                            responseFields(
+                                    fieldWithPath("id").description("The id of the soda."),
+                                    fieldWithPath("version").description("The version of the returned soda."),
+                                    fieldWithPath("createdDate").description("The timestamp at which the soda is saved."),
+                                    fieldWithPath("lastModifiedDate").description("The last timestamp at which the soda data is updated."),
+                                    fieldWithPath("sodaName").description("The name of the soda."),
+                                    fieldWithPath("sodaStyle").description("The type of the soda (more details about the soda name)."),
+                                    fieldWithPath("upc").description("The unique packaging code for each soda bottle."),
+                                    fieldWithPath("price").description("The price of the soda bottle."),
+                                    fieldWithPath("quantityOnHand").description("The quantity of this type of the soda bottle in the inventory.")
+                                    )
+                ));
     }
 
     @Test
